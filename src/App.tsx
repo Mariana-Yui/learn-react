1e13ba934dacd78f7d15100dd70db200401cf7b7
denislin <denislin@tencent.com>
2022-01-28 22:08:42 +0800

    feat: 全局事件总线

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,33 @@
 import React, { PureComponent } from 'react';
-import { IStates } from './types/App';
+import { EventEmitter } from 'events';
 
-export class App extends PureComponent<any, IStates> {
-  constructor(props) {
-    super(props);
-    this.state = {
-      friends: [
-        { id: 1, name: 'lilei', age: 20 },
-        { id: 2, name: 'hanmei', age: 21 },
-        { id: 3, name: 'lihua', age: 22 },
-      ],
-    };
+const eventBus = new EventEmitter();
+export class App extends PureComponent {
+  componentDidMount() {
+    eventBus.addListener('showArgs', this.showArgs);
+  }
+  componentWillUnmount() {
+    eventBus.removeListener('showArgs', this.showArgs);
   }
   render() {
     return (
       <div>
-        <h2>用户资料</h2>
-        <ul>
-          {this.state.friends.map((friend, index) => {
-            return (
-              <li key={friend.id}>
-                姓名: {friend.name}
-                年龄: {friend.age}
-                <button onClick={() => this.addAge(index)}>年龄+1</button>
-              </li>
-            );
-          })}
-        </ul>
-        <button onClick={() => this.increment()}>新增资料</button>
+        <h2>Hello </h2>
+        <button onClick={() => this.trigger()}>触发事件总线</button>
       </div>
     );
   }
-  increment() {
-    const newFriends = [...this.state.friends];
-    newFriends.push({ id: newFriends[newFriends.length - 1].id + 1, name: 'lucy', age: 23 });
-    this.setState({
-      friends: newFriends,
-    });
+  trigger() {
+    eventBus.emit('showArgs', 'React', 2022);
   }
-  addAge(index) {
-    const newFriends = [...this.state.friends];
-    newFriends[index].age += 1;
-    this.setState({
-      friends: newFriends,
-    });
-  }
+  // showArgs(...args) {
+  //   console.log(this);
+  //   console.log('args:', ...args);
+  // }
+  showArgs = (...args) => {
+    console.log(this);
+    console.log('args:', ...args);
+  };
 }
 
 export default App;
