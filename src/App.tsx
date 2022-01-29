bd729bf12dcb1f3d3dcbe413cad705185daf2efc
denislin <denislin@tencent.com>
2022-01-29 22:28:44 +0800

    feat: Fragment

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,16 @@
-import React, { PureComponent } from 'react';
-import { createPortal } from 'react-dom';
-import './style.css';
+import React, { Fragment, PureComponent } from 'react';
 
-class Modal extends PureComponent<any, any> {
-  private el: HTMLDivElement = document.createElement('div');
-  componentDidMount() {
-    document.body.appendChild(this.el);
-  }
-  componentWillUnmount() {
-    document.body.removeChild(this.el);
-  }
-
-  render(): React.ReactNode {
-    return createPortal(this.props.children, this.el);
-  }
-}
 export class App extends PureComponent {
+  constructor(props) {
+    super(props);
+    console.log('App constructor');
+  }
   render() {
     return (
-      <div>
+      // 短语法<> </>
+      <Fragment>
         <h2>Hello React</h2>
-        <Modal>
-          <h2 className="center">Modal</h2>
-        </Modal>
-      </div>
+      </Fragment>
     );
   }
 }
