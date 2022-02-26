46aad27163400ed75fe867de465d52f6abc085f5
denislin <denislin@tencent.com>
2022-02-26 22:30:55 +0800

    chore: reset project

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,5 @@
-import React, { useState } from 'react';
-import ComplexStateHook from './hooks/01_useState';
-import MultiEffectHook from './hooks/02_useEffect';
-import UseContext from './hooks/03_useContext';
-import UseReducer from './hooks/04_useReducer';
-import UseCallback from './hooks/05_useCallback';
-import UseMemo from './hooks/06_useMemo';
-import UseRef from './hooks/07_useRef';
-import UseImperativeHandle from './hooks/08_useImperativeHandle';
-import UseLayoutEffect from './hooks/09_useLayoutEffect';
-import CustomDataStoreHook from './hooks/10_自定义hook/01_instance';
-import ShowScrollHook from './hooks/10_自定义hook/02_instance';
-import UserHook from './hooks/10_自定义hook/03_instance';
+import React from 'react';
 
 export default function App() {
-  const [show, setShow] = useState(true);
-  return (
-    <div>
-      <ComplexStateHook />
-      <>
-        {show && <MultiEffectHook />}
-        <button style={{ marginTop: 5 }} onClick={(e) => setShow(!show)}>
-          展示/隐藏MultiEffectJHook
-        </button>
-      </>
-      <UseContext />
-      <UseReducer />
-      <UseCallback />
-      <UseMemo />
-      <UseRef />
-      <UseImperativeHandle />
-      <UseLayoutEffect />
-      <CustomDataStoreHook />
-      <ShowScrollHook />
-      <UserHook />
-    </div>
-  );
+  return <div>App</div>;
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import React, { createContext } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import 'antd/dist/antd.less';
@@ -6,16 +6,9 @@ import 'moment/dist/locale/zh-cn';
 import App from '@/App';
 import { BrowserRouter } from 'react-router-dom';
 
-export const UserContext = createContext({ name: '', age: -1 });
-export const ThemeContext = createContext('');
-
 ReactDOM.render(
-  <UserContext.Provider value={{ name: 'denis', age: 18 }}>
-    <ThemeContext.Provider value={'tencent'}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </ThemeContext.Provider>
-  </UserContext.Provider>,
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
   document.getElementById('root'),
 );
